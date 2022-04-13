e27ed5bc836ec02807eca24b6e7934f25e0b310d
pengpeng <s123171293@gmail.com>
2022-04-13 22:46:27 +0800

    weather api use key

---
+++ apilearn.js
@@ -0,0 +1,21 @@
+// endpoint  ,path, query
+
+/*
+async function getJoke() {
+  let data = await fetch("https://v2.jokeapi.dev/joke/Programming?type=single");
+  let parsedData = await data.json();
+  console.log(parsedData);
+}
+getJoke();
+*/
+let key = "9baa373013ff1f2c97362c1f2c7a13f5";
+let city = prompt("ennter the city name");
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
+
+async function getWeather() {
+  let data = await fetch(url);
+  let dj = await data.json();
+  console.log(dj);
+}
+
+getWeather();
